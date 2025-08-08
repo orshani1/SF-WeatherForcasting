0f16f77879f36e3b6bf3fb4f0545a5f687ef10ad
OrtheOG <ortheog@gmail.com>
2025-08-08 20:08:19 +0300

    part 2

--- WeatherForcast/force-app/main/default/lwc/weatherForecastDetail/weatherForecastDetail.js
+++ WeatherForcast/force-app/main/default/lwc/weatherForecastDetail/weatherForecastDetail.js
@@ -14,7 +14,12 @@ export default class WeatherForecastDetail extends LightningElement {
 
     weatherWindIcon = 'https://cdn-icons-png.flaticon.com/512/6143/6143028.png';
 
-
+    renderedCallback(){
+        if(this.forecast && this.forecast.location){
+            console.log('### location = ' + JSON.stringify(this.forecastDaysArray[0].day));
+            
+        }
+    }
 
     get isDayRendered(){
             return this.forecastDay && this.forecastDay.day && this.forecastDay.day.condition && this.forecastDay.day.condition.icon  && this.forecast && this.forecast.location;        
--- WeatherForcast/force-app/main/default/lwc/weatherForecastingTable/weatherForecastingTable.js
+++ WeatherForcast/force-app/main/default/lwc/weatherForecastingTable/weatherForecastingTable.js
@@ -4,5 +4,29 @@ export default class WeatherForecastingTable extends LightningElement {
      
     @api forcastDaysArray;
 
-    
+    async connectedCallback(){
+        this.forcastDaysArray = await this.forcastDaysArray.map(forecastDay =>{
+            return {...forecastDay,
+                        day:{
+                            mintempx5fc:forecastDay.day.mintempx5fc,
+                            maxtempx5fc:forecastDay.day.mintempx5fc,
+                            condition:{
+                                ...forecastDay.day.condition,
+                                class:(forecastDay.day.condition.text.includes('Cloudy') ? 'table-row blue' : forecastDay.day.condition.text.includes('Sunny') ? 'table-row orange' : 'table-row gray')
+                            }
+                        }
+            
+            };
+        });
+    }
+
+    handleDaySelection(e){
+        debugger;
+        console.log('e = ' + JSON.stringify(e.detail));
+        console.log('e target = ' + JSON.stringify(e.target));
+        debugger;
+
+        
+    }
+
 }
